89eeeede0df82f8403bfb96710c56aec37d701ef
biFebriansyah <bukanebi@gmail.com>
2019-12-18 13:47:06 +0700

    Before install

--- src/screen/Home/Home.js
+++ src/screen/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StatusBar, Image } from 'react-native';
+import { Text, View, StatusBar, Image, ScrollView } from 'react-native';
 import { Header } from 'native-base';
 import Color from '../../global/style/Color';
 import IconMaterial from 'react-native-vector-icons/MaterialIcons';
@@ -11,19 +11,28 @@ class Home extends Component {
                 <StatusBar backgroundColor={Color.primary} />
                 <Header androidStatusBarColor={Color.primary} style={{ backgroundColor: Color.acent, alignItems: 'center', justifyContent: 'flex-start' }}>
                     <View style={{ width: 120, height: 44 }}>
-                        <Image source={require('../../global/asset/logo.png')} style={{ width: '100%', height: '100%', resizeMode: 'contain', flex:1 }} />
+                        <Image source={require('../../global/asset/logo.png')} style={{ width: '100%', height: '100%', resizeMode: 'contain', flex: 1 }} />
                     </View>
                     <IconMaterial name='person' size={24} color={Color.primary} />
                 </Header>
                 <View style={{ width: '100%', backgroundColor: '#bbb', height: 214 }}>
                     <Image source={require('../../global/asset/hero1-edited.jpg')} style={{ width: '100%', height: '100%', resizeMode: 'cover', }} />
                 </View>
-                <View style={{ height: 245 }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, paddingTop: 10, alignItems: 'center' }}>
+                <View style={{ height: 245, marginTop: 20 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 16, alignItems: 'center' }}>
                         <Text style={{ fontSize: 20, fontFamily: 'Roboto-Bold' }}>Promo</Text>
                         <Text style={{ color: Color.primary, fontFamily: 'Roboto', fontSize: 15, fontWeight: '600' }}>Lihat Semua</Text>
                     </View>
-                    <View></View>
+                    <ScrollView horizontal>
+                        <View style={{ alignItems: 'center', marginTop: 25, flexDirection: 'row' }}>
+                            <View style={{ backgroundColor: '#bbb', width: 294, height: 158, borderRadius: 10, marginRight: 50 }}>
+                                <Image source={require('../../global/asset/Promo1.jpg')} style={{ width: null, height: null, resizeMode: 'cover', flex: 1, borderRadius: 10 }} />
+                            </View>
+                            <View style={{ backgroundColor: '#bbb', width: 294, height: 158, borderRadius: 10 }}>
+                                <Image source={require('../../global/asset/Promo1.jpg')} style={{ width: null, height: null, resizeMode: 'cover', flex: 1, borderRadius: 10 }} />
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </>
         )
--- src/screen/Inbox/Inbox.js
+++ src/screen/Inbox/Inbox.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { Header } from 'native-base';
 import Color from '../../global/style/Color';
+import { SliderBox } from 'react-native-image-slider-box';
 
 export class Inbox extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            images: [
+                require('../../global/asset/Promo1.jpg'),
+                require('../../global/asset/Promo2.jpg'),
+                require('../../global/asset/Promo3.jpg'),
+                require('../../global/asset/Promo4.jpg'),
+                require('../../global/asset/Promo5.jpg'),
+                require('../../global/asset/Promo6.jpg'),
+            ]
+        }
+    }
+
+    onLayout = e => {
+        this.setState({
+            width: e.nativeEvent.layout.width
+        });
+    };
+
     render() {
         return (
             <>
@@ -13,9 +35,32 @@ export class Inbox extends Component {
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <Text> Inbox Here </Text>
                 </View>
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={styles.container} onLayout={this.onLayout}>
+                        <SliderBox
+                            images={this.state.images}
+                            dotColor="#000"
+                            ImageComponent ={
+                                <Image  />
+                            }
+                            onCurrentImagePressed={index =>
+                                console.warn(`image ${index} pressed`)
+                            }
+                            parentWidth={this.state.width}
+                        />
+                    </View>
+                </View>
             </>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: 294,
+        height: 158,
+        borderRadius: 10
+    }
+});
 
 export default Inbox
