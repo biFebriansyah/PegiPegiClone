a17e1049f64216bb646044f13704e3ddc411ecca
syifamohnaufal <syifamnaufal@gmail.com>
2019-12-20 16:47:43 +0700

    finish pembayaran uye

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import Routes from './src/screen/Routes';
 
+import Pembayaran from './src/components/Pembayaran/Pembayaran'
+
 class App extends Component {
   render() {
-    return <Routes />;
+    return <Pembayaran />;
   }
 }
 
